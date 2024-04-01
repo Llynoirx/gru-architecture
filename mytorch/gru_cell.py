00bde4142543d9c2c0e9948949e7314651744fed
llynoirx <kathyho04@gmail.com>
2024-03-31 22:28:20 -0400

    finally works

--- mytorch/gru_cell.py
+++ mytorch/gru_cell.py
@@ -20,13 +20,13 @@ class GRUCell(object):
         self.Wzh = np.random.randn(h, h)
         self.Wnh = np.random.randn(h, h)
 
-        self.brx = np.random.randn(h)
-        self.bzx = np.random.randn(h)
-        self.bnx = np.random.randn(h)
+        self.brx = np.random.randn(h) 
+        self.bzx = np.random.randn(h) 
+        self.bnx = np.random.randn(h) 
 
-        self.brh = np.random.randn(h)
-        self.bzh = np.random.randn(h)
-        self.bnh = np.random.randn(h)
+        self.brh = np.random.randn(h) 
+        self.bzh = np.random.randn(h) 
+        self.bnh = np.random.randn(h) 
 
         self.dWrx = np.zeros((h, d))
         self.dWzx = np.zeros((h, d))
@@ -91,6 +91,8 @@ class GRUCell(object):
         # Define your variables based on the writeup using the corresponding
         # names below.
 
+        self.n_state = np.dot(self.Wnh, h_prev_t) + self.bnh 
+        
         self.r = self.r_act.forward(np.dot(self.Wrx, x) + self.brx + np.dot(self.Wrh, h_prev_t) + self.brh) 
         self.z = self.z_act.forward(np.dot(self.Wzx, x) + self.bzx + np.dot(self.Wzh, h_prev_t) + self.bzh) 
         self.n = self.h_act.forward(np.dot(self.Wnx, x) + self.bnx + self.r*(np.dot(self.Wnh, h_prev_t) + self.bnh)) 
@@ -134,40 +136,55 @@ class GRUCell(object):
         # 2) When in doubt about shapes, please refer to the table in the writeup.
         # 3) Know that the autograder grades the gradients in a certain order, and the local autograder will tell you which gradient you are currently failing. 
 
-        # Ensure 2D col vectors w/ shape: (1, hidden)
-        delta = delta.reshape(1, -1) 
-        self.x = self.x.reshape(1, -1)  
-        self.hidden = self.hidden.reshape(1, -1)  
 
-        # Initialize derivatives 
+        # delta = delta.reshape(1, -1)  # (1, hidden_dim)
+        self.x = self.x.reshape(1, -1)  
+        self.hidden = self.hidden.reshape(1, -1) 
+        self.r = self.r.reshape(1, -1)  
+        self.z = self.z.reshape(1, -1)  
+        self.n = self.n.reshape(1, -1)   
+
         dx = np.zeros_like(self.x, dtype=np.float64)
         dh_prev_t = np.zeros_like(self.hidden, dtype=np.float64)
+        dn = np.zeros_like(self.n, dtype=np.float64)
+        dz = np.zeros_like(self.z, dtype=np.float64)
+        dr = np.zeros_like(self.r, dtype=np.float64)
 
-        # Compute derivatives of gates
-        dz = delta * (self.hidden - self.n)  # (1, hidden_dim)
-        dr = delta * self.z * (1 - self.z) * (self.n * (1 - np.tanh(self.n)**2))
-        dn = (delta * (1 - self.z) * (1 - np.tanh(self.n)**2)) * self.r
+        dz += delta * (self.hidden - self.n) 
+        dn += delta * (1 - self.z)
+        dh_prev_t += delta * self.z
 
-        #Update dx and d_{h-1}
-        dh_prev_t += dz.dot(self.Wzh.T) + dr.dot(self.Wrh.T) + dn.dot(self.Wnh.T)
-        dx = dz.dot(self.Wzx) + dr.dot(self.Wrx) + dn.dot(self.Wnx)
-        
-        # Calculate derivatives wrt weights
-        self.dWrx = np.dot(dr.T, self.x)
-        self.dWzx = np.dot(dz.T, self.x)
+        dn_actftn = dn * (1-self.n**2)
+        dn_actftn_r = dn_actftn * self.r
 
-        self.dWnx = np.dot(dn.T, self.x)
-        self.dWrh = np.dot(dr.T, self.hidden)
-        self.dWzh = np.dot(dz.T, self.hidden)
-        self.dWnh = np.dot(dn.T, self.hidden)
+        self.dWnx += np.dot(dn_actftn.T, self.x)
+        dx += np.dot(dn_actftn, self.Wnx)
+        self.dbnx += np.sum(dn_actftn, axis=0)
+        dr += dn_actftn * self.n_state.T
 
-        # Calculate derivatives wrt biases
-        self.dbrx = np.sum(dr, axis=0)
-        self.dbzx = np.sum(dz, axis=0)
-        self.dbnx = np.sum(dn, axis=0)
-        self.dbrh = self.dbrx
-        self.dbzh = self.dbzx
-        self.dbnh = np.sum(dn * self.r, axis=0)
+        self.dWnh += np.dot(dn_actftn_r.T, self.hidden)
+        dh_prev_t += np.dot(dn_actftn_r, self.Wnh)
+        self.dbnh += np.sum(dn_actftn_r, axis=0)
+
+        dz_actftn = dz * self.z * (1-self.z)
+
+        dx += np.dot(dz_actftn, self.Wzx)
+        self.dWzx += np.dot(dz_actftn.T, self.x)
+        self.dWzh += np.dot(dz_actftn.T, self.hidden)
+        dh_prev_t += np.dot(dz_actftn, self.Wzh)
+        self.dbzx += np.sum(dz_actftn, axis=0)
+        self.dbzh += np.sum(dz_actftn, axis=0)
+
+        dr_actftn = dr * self.r * (1-self.r)
+        dx += np.dot(dr_actftn, self.Wrx)
+        self.dWrx += np.dot(dr_actftn.T, self.x)
+        self.dWrh += np.dot(dr_actftn.T, self.hidden)
+        dh_prev_t += np.dot(dr_actftn, self.Wrh)
+        self.dbrx += np.sum(dr_actftn, axis=0)
+        self.dbrh += np.sum(dr_actftn, axis=0)
+
+        # dh_prev_t += dz.dot(self.Wzh.T) + dr.dot(self.Wrh.T) + dn.dot(self.Wnh.T)
+        # dx = dz.dot(self.Wzx) + dr.dot(self.Wrx) + dn.dot(self.Wnx)
 
         # Ensure correct output shapes
         dx = dx.reshape(-1)
